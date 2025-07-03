6e26bca9e73ac88620b41336838eb60cb6e5b1ea
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-07-03 16:58:38 +0530

    Mail service created?

--- internal/mail/mail-service.go
+++ internal/mail/mail-service.go
@@ -1,15 +1,21 @@
 package mail
 
+import (
+	"log"
+)
+
+// we can use this mail queue and the struct EmailRequest to send request to mail service
 var MailQueue = make(chan EmailRequest)
 
 func StartMailWorkerPool(n int) {
-	for i:=0; i<n; i++ {
+	for range n {
 		go func() {
 			for req:= range MailQueue {
 				if err:=SendMail(req.To, req.Subject, req.Type, req.Data); err!=nil {
-					
+					//TODO: change this to logger
+					log.Println("cannot send mail, error occured: %w", err)
 				}
 			}
-		}
+		}()
 	}
 }
