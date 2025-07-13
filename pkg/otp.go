6acddbebe5483e354ef66ee2e4b7830a0d1f9524
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-07-14 01:37:15 +0530

    Removed TODO logs

--- pkg/otp.go
+++ pkg/otp.go
@@ -10,7 +10,6 @@ func GenerateOTP() (int, error) {
 	max := big.NewInt(10000)
 	n, err := rand.Int(rand.Reader, max)
 	if err != nil {
-		//TODO: Change this to Logger when logger branch is merged..for now using fmt
 		return 0, fmt.Errorf("failed to generate OTP: %w", err)
 	}
 	return int(n.Int64()), nil
