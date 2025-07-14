e8b275d9c427278e9db27ae7ff7956c018bf9fd1
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-15 03:51:10 +0530

    merge fixes

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"fmt" "log" "net/http"
 
 	"github.com/Thanus-Kumaar/anokha-2025-backend/cmd"
 	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
@@ -19,13 +17,7 @@ func main() {
 	cmd.Env = config
 	log.Println("Environment variables loaded successfully.")
 
-	err = cmd.InitDBPool()
-	if err != nil {
-		panic(fmt.Errorf("failed to initialize database pool: %w", err))
-	}
 
-	r := gin.New()
-	r.Use(gin.Logger())
 
 	// initializing the logger and other middlewares
 	logger, err := cmd.InitLogger("DEVELOPMENT") // NOTE: hardcoded now, will change once viper is setup
@@ -35,14 +27,31 @@ func main() {
 	pkg.Log = logger
 	pkg.Log.LogInfo("Logger initiation successful")
 
-	r.Use(pkg.Log.LogRequest)
+	// Initialize RSA
+	err = cmd.CheckRSAKeyPairExists()
+	if err != nil {
+		err = cmd.GenerateRSAKeyPair()
+		if err != nil {
+			panic(fmt.Errorf("Failed to initialize RSA: %w", err))
+		}
+		pkg.Log.LogInfo("[OK]: RSA keypair generated and saved successfully.")
+	} else {
+		pkg.Log.LogInfo("[OK]: Using existing RSA keypair.")
+	}
 
+	err = cmd.InitDBPool()
+	if err != nil {
+		panic(fmt.Errorf("failed to initialize database pool: %w", err))
+	}
+
+
+	r.Use(pkg.Log.LogRequest)
+	r := gin.New()
 	r.GET("/api/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Server is live ◪_◪",
 		})
 	})
-
 	err = r.Run(":" + "9000")
 	if err != nil {
 		fmt.Println("Server failed")
