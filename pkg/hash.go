0b71bcc0007fa86aab2def3976b672ad36d32390
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-17 03:31:29 +0530

    test: Added test cases for hash function

--- pkg/hash.go
+++ pkg/hash.go
@@ -5,7 +5,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func GenerateHashOfPassword(password string) (string, error) {
+func Hash(password string) (string, error) {
 	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("unable to generate hash %w", err)
