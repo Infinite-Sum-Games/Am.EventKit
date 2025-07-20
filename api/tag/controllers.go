63fcf92f93bda65dcd8585d87cb442c5a15b9c44
Akshay KS <143313500+akshayks13@users.noreply.github.com>
2025-07-20 23:08:44 +0530

    Update api/tag/controllers.go
    Co-authored-by: Thanus Kumaar <111196217+Thanus-Kumaar@users.noreply.github.com>

--- api/tag/controllers.go
+++ api/tag/controllers.go
@@ -25,7 +25,7 @@ func GetAllTags(c *gin.Context) {
 
 	tags, err := q.ListTags(ctx, conn)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch tags"})
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch tags"})
 		pkg.Log.ErrorCtx(c, "[TAG-ERROR]: Failed to fetch tags", err)
 		return
 	}
