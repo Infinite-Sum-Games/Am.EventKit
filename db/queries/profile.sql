5542a329c5a3a457bcd8930a5b8f82dfb086df77
Nandgopal-R <nandgopalrnair@gmail.com>
2025-07-21 23:56:50 +0530

    feat: add sqlc query for fetching profile information

---
+++ db/queries/profile.sql
@@ -0,0 +1,5 @@
+-- name: ListProfileInfo :one
+SELECT name, department_name, email, phone_number, is_amrita_student, 
+amrita_roll_number, college_name, college_city, academic_year, account_status 
+FROM student 
+WHERE id = $1;
