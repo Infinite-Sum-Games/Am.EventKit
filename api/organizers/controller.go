0f64d4de9642e539da50057b54901f408128b289
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2025-07-23 17:32:13 +0530

    added controllers.go for feat/organizers to fetchAllOrganizers

--- api/organizers/controller.go
+++ api/organizers/controller.go
@@ -1,1 +1,40 @@
 package organizers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Thanus-Kumaar/anokha-2025-backend/cmd"
+	db "github.com/Thanus-Kumaar/anokha-2025-backend/db/gen"
+	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllOrganizers(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to acquire DB connection"})
+		pkg.Log.ErrorCtx(c, "[ORGANIZER-ERROR]: Failed to acquire DB connection", err)
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+
+	organizers, err := q.ListOrganizersQuery(ctx, conn)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch organizers"})
+		pkg.Log.ErrorCtx(c, "[ORGANIZER-ERROR]: Failed to fetch organizers", err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message":    "Organizers list fetched successfully",
+		"organizers": organizers,
+	})
+	pkg.Log.SuccessCtx(c)
+
+}
