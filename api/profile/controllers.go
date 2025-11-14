6d235cd69774302c38d4f4b6189236cc7cca42d8
Nandgopal-R <nandgopalrnair@gmail.com>
2025-11-14 23:00:45 +0530

    feat: add function to edit profile

--- api/profile/controllers.go
+++ api/profile/controllers.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"net/http"
 
+	"time"
+
 	"github.com/Thanus-Kumaar/anokha-2025-backend/cmd"
 	db "github.com/Thanus-Kumaar/anokha-2025-backend/db/gen"
+	mw "github.com/Thanus-Kumaar/anokha-2025-backend/middleware"
+	"github.com/Thanus-Kumaar/anokha-2025-backend/models"
 	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
-	"time"
 )
 
 func FetchUserProfile(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// TODO - Replace with email retireved from auth token
+	// TODO - Replace with email retrieved from auth token
 	email := "sample@gmail.com" // For testing purposes
 
 	conn, err := cmd.DBPool.Acquire(ctx)
@@ -49,14 +52,73 @@ func FetchUserProfile(c *gin.Context) {
 }
 
 func EditUserProfileCsrf(c *gin.Context) {
+	// TODO - Replace with email retireved from auth token
+	email := "sample@gmail.com" // For testing purposes
+
+	csrfToken := pkg.CreateCsrfToken(email, "edit_profile")
+
+	pkg.SetCsrfCookie(c, csrfToken)
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User profile update initiated",
-		"key":     "",
+		"key":     csrfToken,
 	})
 	pkg.Log.SuccessCtx(c)
 }
 
 func EditUserProfile(c *gin.Context) {
+
+	mw.VerifyCsrf(c)
+
+	// TODO - Replace with email retrieved from auth token
+	email := "sample@gmail.com" // For testing purposes"
+
+	var req models.EditProfileRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request payload"})
+		pkg.Log.ErrorCtx(c, "[PROFILE-ERROR]: Invalid request payload", err)
+		return
+	}
+
+	if err := req.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Validation error", "errors": err.Error()})
+		pkg.Log.ErrorCtx(c, "[PROFILE-ERROR]: Validation error", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		pkg.Log.ErrorCtx(c, "[PROFILE-ERROR]: Failed to acquire DB connection", err)
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+
+	row, err := q.EditUserProfileQuery(ctx, conn, db.EditUserProfileQueryParams{
+		Email:          email,
+		Name:           req.Name,
+		DepartmentName: req.DepartmentName,
+		PhoneNumber:    req.PhoneNumber,
+		CollegeName:    req.CollegeName,
+		CollegeCity:    req.CollegeCity,
+		AcademicYear:   req.AcademicYear,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		pkg.Log.ErrorCtx(c, "[PROFILE-ERROR]: Failed to edit user profile", err)
+		return
+	} else if row == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "User profile does not exist"})
+		pkg.Log.ErrorCtx(c, "[PROFILE-ERROR]: User profile does not exist", nil)
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User profile updated successfully",
 	})
--- models/profile.go
+++ models/profile.go
@@ -1,9 +1,27 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"regexp"
 
-type Profile struct {
-	ID       uuid.UUID `json:"user_id"`
-	Email    string    `json:"user_email"`
-	FullName string    `json:"user_full_name"`
+	v "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type EditProfileRequest struct {
+	Name           string `json:"name"`
+	DepartmentName string `json:"department_name"`
+	PhoneNumber    string `json:"phone_number"`
+	CollegeName    string `json:"college_name"`
+	CollegeCity    string `json:"college_city"`
+	AcademicYear   string `json:"academic_year"`
+}
+
+func (e EditProfileRequest) Validate() error {
+	return v.ValidateStruct(&e,
+		v.Field(&e.Name, v.Required, v.Length(2, 100)),
+		v.Field(&e.DepartmentName, v.Required, v.Length(2, 100)),
+		v.Field(&e.PhoneNumber, v.Required, v.Match(regexp.MustCompile(`^[0-9]{10}$`))),
+		v.Field(&e.CollegeName, v.Required, v.Length(2, 200)),
+		v.Field(&e.CollegeCity, v.Required, v.Length(2, 100)),
+		v.Field(&e.AcademicYear, v.Required, v.In("1", "2", "3", "4", "5")),
+	)
 }
