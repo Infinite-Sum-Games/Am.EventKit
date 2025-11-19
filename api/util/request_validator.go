8cb2f9f663542ba45248eb4d7ab272ad948ff2e8
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-20 00:30:47 +0530

    Schema altered and pushed

--- api/util/request_validator.go
+++ api/util/request_validator.go
@@ -13,11 +13,13 @@ type Validatable interface {
 
 func ValidateRequest[T Validatable](c *gin.Context) (*T, bool) {
 	var req T
+
 	if err := c.BindJSON(&req); err != nil {
 		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to bind JSON", err)
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
 		return nil, false
 	}
+
 	if err := req.Validate(); err != nil {
 		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Validation failed", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
