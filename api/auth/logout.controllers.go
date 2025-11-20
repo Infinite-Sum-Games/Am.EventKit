34cea0bd20c1622be5b8ff1e457ca07ac9bc280d
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-20 17:00:40 +0530

    feat: Logout Routes completed

---
+++ api/auth/logout.controllers.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
+	"github.com/gin-gonic/gin"
+)
+
+func LogoutUser(c *gin.Context) {
+	pkg.NullifyCookies(c)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User logged out successfully",
+	})
+
+	pkg.Log.SuccessCtx(c)
+}
+
+func LogoutOrganizer(c *gin.Context) {
+	pkg.NullifyCookies(c)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Organizer logged out successfully",
+	})
+
+	pkg.Log.SuccessCtx(c)
+}
