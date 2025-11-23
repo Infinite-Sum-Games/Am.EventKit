d8f92427afa27d919ee24d030a76f6b14afc7f97
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-23 15:39:17 +0530

    chore: Formatting fixes to respect 80 character limit

--- api/tag/controllers.go
+++ api/tag/controllers.go
@@ -17,7 +17,9 @@ func GetAllEventTags(c *gin.Context) {
 
 	conn, err := cmd.DBPool.Acquire(ctx)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
 		pkg.Log.ErrorCtx(c, "[TAG-ERROR]: Failed to acquire DB connection", err)
 		return
 	}
@@ -27,10 +29,14 @@ func GetAllEventTags(c *gin.Context) {
 
 	tags, err := q.ListTagsQuery(ctx, conn)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+
 		pkg.Log.ErrorCtx(c, "[TAG-ERROR]: Failed to fetch tags", err)
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Tags list fetched successfully",
 		"tags":    tags,
