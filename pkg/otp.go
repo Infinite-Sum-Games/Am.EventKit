538581898ccc51aad744b358375be0d0f8d30bbf
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-23 15:55:54 +0530

    Added comments to explain steps of OTP generation function

--- pkg/otp.go
+++ pkg/otp.go
@@ -9,19 +9,26 @@ import (
 
 var otpMax = big.NewInt(10000)
 
+// Totally over-engineered OTP generation function :(
 func GenerateOTP() (string, []string, error) {
 	if otpMax.Cmp(big.NewInt(0)) <= 0 {
 		return "", []string{}, fmt.Errorf("failed to generate OTP: max must be > 0 (got %s)", otpMax.String())
 	}
+
 	n, err := rand.Int(rand.Reader, otpMax)
 	if err != nil {
 		return "", []string{}, fmt.Errorf("failed to generate OTP: %w", err)
 	}
+
+	// Convert OTP from Int64 to Int
 	otp := int(n.Int64())
+
+	// Get a string & []string from Int
 	otpStr := strconv.Itoa(otp)
 	otpSlice := make([]string, len(otpStr))
 	for i, ch := range otpStr {
 		otpSlice[i] = string(ch)
 	}
+
 	return otpStr, otpSlice, nil
 }
