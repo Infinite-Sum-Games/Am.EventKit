61bfb668a5ac7dd9e7d38e05c802ccaefb3a63f7
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-24 03:26:22 +0530

    Add authenticated GET routes

--- api/event/routes.go
+++ api/event/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	mw "github.com/Thanus-Kumaar/anokha-2025-backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,12 +9,15 @@ func EventRoutes(r *gin.RouterGroup) {
 	r.GET("/", FetchAllEvents)
 	r.GET("/:eventId", FetchEventById)
 
+	r.GET("/auth/", FetchAllEventsWithAuth)
+	r.GET("/auth/:eventId", FetchEventByEventIdWithAuth)
+
 	r.POST("/register/:eventId")
 	r.GET("/register/:eventId")
 
 	r.GET("/tags", GetAllEventTags)
 	r.GET("/organizers", GetAllOrganizers)
 
-	r.PUT("/favourite/:eventId", StarEvent)
-	r.DELETE("/favourite/:eventId", UnstarEvent)
+	r.PUT("/favourite/:eventId", mw.Auth, StarEvent)
+	r.DELETE("/favourite/:eventId", mw.Auth, UnstarEvent)
 }
