8c7103e5e0a8bd2dea0553ea162169806ff6eb71
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-24 01:20:46 +0530

    Remove errors package import

--- api/auth/login.controllers.go
+++ api/auth/login.controllers.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"time"
 
@@ -16,12 +15,7 @@ import (
 )
 
 func LoginUserCsrf(c *gin.Context) {
-	req, ok := pkg.ValidateRequest[models.LoginRequest](c)
-	if !ok {
-		return
-	}
-
-	csrfToken, err := pkg.CreateCsrfToken(req.Email, c)
+	csrfToken, err := pkg.CreateCsrfToken("login.user@amrita.edu", c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Oops! Something happened. Please try again later.",
@@ -48,67 +42,96 @@ func LoginUser(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	conn, err := cmd.DBPool.Acquire(ctx)
+	tx, err := cmd.DBPool.Begin(ctx)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Oops! Something happened. Please try again later",
 		})
 
-		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to acquire DB connection", err)
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to initiate DB transaction", err)
 		return
 	}
-	defer conn.Release()
+	defer tx.Rollback(ctx)
 
 	q := db.New()
-	user, err := q.CheckStudentVerifiedQuery(ctx, conn, req.Email)
+	password, err := pkg.Hash(req.HashedPassword)
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			c.JSON(http.StatusNotFound, gin.H{"message": "User not found. Please register first."})
-			return
-		}
-		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to fetch user", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
-		return
-	}
-	if req.HashedPassword != user.Password {
-		c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid password for given email"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "",
+		})
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to hash password", err)
 		return
 	}
 
-	var refreshToken string
-	isNewToken := false
-	if user.RefreshToken.String == "" {
-		// no token exists, create new refresh token and set it in database
-		refreshToken = pkg.CreateRefreshToken(user.ID.String(), user.Email, true, false)
-		isNewToken = true
-	} else {
-		// Validate existing token
-		valid, _ := pkg.ParseToken(user.RefreshToken.String, "refresh_token")
-		if !valid {
-			refreshToken = pkg.CreateRefreshToken(user.ID.String(), user.Email, true, false)
-			isNewToken = true
-		} else {
-			refreshToken = user.RefreshToken.String
-		}
+	result, err := q.LoginUserQuery(ctx, tx, db.LoginUserQueryParams{
+		Email:    req.Email,
+		Password: password,
+	})
+	if err == pgx.ErrNoRows {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No user with given email and password",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Username and password do not exist", err)
+		return
 	}
-	if isNewToken {
-		err = q.UpdateRefreshTokenQuery(ctx, conn, db.UpdateRefreshTokenQueryParams{
-			ID: user.ID,
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to fetch login details", err)
+		return
+	}
+
+	// If there is no refreshToken then create one, add it to the database and
+	// proceed to mint auth token, set the cookies and send back the response
+	if result.RefreshToken.String == "" {
+		refreshToken, err := pkg.CreateRefreshToken(result.ID.String(), result.Email, true, false)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Oops! Something happened. Please try again later.",
+			})
+			pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to create refresh token", err)
+			return
+		}
+
+		token, err := q.UpdateRefreshTokenQuery(ctx, tx, db.UpdateRefreshTokenQueryParams{
+			Email: req.Email,
 			RefreshToken: pgtype.Text{
 				String: refreshToken,
-				Valid:  refreshToken != "",
+				Valid:  true,
 			},
 		})
 		if err != nil {
-			pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to update refresh token", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Oops! Something happened. Please try again later.",
+			})
+			pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Could not add refresh token to DB", err)
 			return
 		}
+
+		pkg.SetRefreshCookie(c, token.String)
 	}
-	// createing auth token and setting both tokens as cookies
-	authToken := pkg.CreateAuthToken(user.ID.String(), user.Name, user.Email, true, false, false)
+
+	if err := tx.Commit(ctx); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to commit transaction", err)
+		return
+	}
+
+	authToken, err := pkg.CreateAuthToken(result.ID.String(), result.Email, true, false)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to create auth token", err)
+		return
+	}
 	pkg.SetAuthCookie(c, authToken)
-	pkg.SetRefreshCookie(c, refreshToken)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User logged in successfully",
@@ -117,12 +140,7 @@ func LoginUser(c *gin.Context) {
 }
 
 func LoginOrganizerCsrf(c *gin.Context) {
-	req, ok := pkg.ValidateRequest[models.LoginRequest](c)
-	if !ok {
-		return
-	}
-
-	csrfToken, err := pkg.CreateCsrfToken(req.Email, c)
+	csrfToken, err := pkg.CreateCsrfToken("login.organizer@amrita.edu", c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Oops! Something happened. Please try again later.",
@@ -140,6 +158,107 @@ func LoginOrganizerCsrf(c *gin.Context) {
 }
 
 func LoginOrganizer(c *gin.Context) {
+	// For organizers, email should be like - <department>@cb.amrita.edu
+	req, ok := pkg.ValidateRequest[models.LoginRequest](c)
+	if !ok {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tx, err := cmd.DBPool.Begin(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to initiate DB transaction", err)
+		return
+	}
+	defer tx.Rollback(ctx)
+
+	q := db.New()
+
+	password, err := pkg.Hash(req.HashedPassword)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to hash password", err)
+		return
+	}
+
+	result, err := q.LoginOrganizerQuery(ctx, tx, db.LoginOrganizerQueryParams{
+		Email:    req.Email,
+		Password: password,
+	})
+	if err == pgx.ErrNoRows {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No organizer with given email and password found",
+		})
+
+		pkg.Log.WarnCtx(c, "[AUTH-WARN]: Invalid email and password")
+		return
+	}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Could not login organizer", err)
+		return
+	}
+
+	// Check if there is a refreshToken already. If it exists, just mint an
+	// auth token, set the cookies and return. Otherwise, mint both and return.
+	if result.RefreshToken.String == "" {
+		refreshToken, err := pkg.CreateRefreshToken(result.ID.String(), req.Email, false, true)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Oops! Something happened. Please try again later.",
+			})
+
+			pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: ", err)
+			return
+		}
+
+		token, err := q.UpdateOrganizerRefreshTokenQuery(ctx, tx,
+			db.UpdateOrganizerRefreshTokenQueryParams{
+				Email:        req.Email,
+				RefreshToken: refreshToken,
+			})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Oops! Something happened. Please try again later.",
+			})
+			pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Could not add refresh token to DB", err)
+			return
+		}
+
+		pkg.SetRefreshCookie(c, refreshToken)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to commit transaction", err)
+		return
+	}
+
+	authToken, err := pkg.CreateAuthToken(result.ID.String(), req.Email, false, true)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+
+		pkg.Log.ErrorCtx(c, "[AUTH-ERROR]: Failed to create auth token", err)
+		return
+	}
+	pkg.SetAuthCookie(c, authToken)
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Organizer logged in successfully",
 	})
