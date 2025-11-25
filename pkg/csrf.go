125882108fb07e8a8d7b86947abcfcfaa2dd894f
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-25 23:07:22 +0530

    fix: Routes in csrf verification

--- pkg/csrf.go
+++ pkg/csrf.go
@@ -9,11 +9,13 @@ import (
 )
 
 var CsrfRoutes = map[string]string{
-	"api/v1/auth/login":           "user.login",
-	"api/v1/auth/register":        "user.register",
-	"api/v1/auth/otp/verify":      "user.otp_verify",
-	"api/v1/auth/organizer/login": "organizer.login",
-	"api/v1/user/profile":         "user.edit_profile",
+	"/api/v1/auth/user/login":                      "user.login",
+	"/api/v1/auth/user/register":                   "user.register",
+	"/api/v1/auth/user/register/otp/verify":        "user.otp_verify",
+	"/api/v1/auth/organizer/login":                 "organizer.login",
+	"/api/v1/auth/user/forgot-password":            "user.forgot_password",
+	"/api/v1/auth/user/forgot-password/otp/verify": "user.forgot_password_otp",
+	"/api/v1/user/profile":                         "user.edit_profile",
 }
 
 func CreateCsrfToken(email string, c *gin.Context) (string, error) {
@@ -25,12 +27,12 @@ func CreateCsrfToken(email string, c *gin.Context) (string, error) {
 	token := paseto.NewToken()
 
 	token.SetJti(email)
+	token.SetSubject("csrf_token")
 	token.SetIssuer("Anokha-25: AUTH-SERVICE")
 	token.SetIssuedAt(time.Now())
 	token.SetNotBefore(time.Now())
 	token.SetExpiration(time.Now().Add(CsrfTokenValidTime))
-
-	token.SetSubject(csrfPurpose)
+	token.SetAudience(csrfPurpose)
 
 	signed := token.V4Sign(SignKey, nil)
 	return signed, nil
