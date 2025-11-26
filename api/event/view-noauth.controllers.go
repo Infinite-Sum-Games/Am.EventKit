4924dc7cfcec110356e0dbc74f74135d64ed09e2
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-11-26 16:27:26 +0530

    Fix return JSON formatting in Event controllers

--- api/event/view-noauth.controllers.go
+++ api/event/view-noauth.controllers.go
@@ -20,7 +20,9 @@ func FetchAllEvents(c *gin.Context) {
 
 	conn, err := cmd.DBPool.Acquire(ctx)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Failed to acquire DB connection", err)
 		return
 	}
@@ -30,7 +32,9 @@ func FetchAllEvents(c *gin.Context) {
 
 	events, err := q.GetEventsQuery(ctx, conn)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Failed to fetch events", err)
 		return
 	}
@@ -46,7 +50,9 @@ func FetchEventById(c *gin.Context) {
 	eventIdStr := c.Param("eventId")
 	eventId, err := uuid.Parse(eventIdStr)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Request not processed due to invalid parameters"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Request not processed due to invalid parameters",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Invalid event ID", err)
 		return
 	}
@@ -56,7 +62,9 @@ func FetchEventById(c *gin.Context) {
 
 	conn, err := cmd.DBPool.Acquire(ctx)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Failed to acquire DB connection", err)
 		return
 	}
@@ -65,14 +73,20 @@ func FetchEventById(c *gin.Context) {
 	q := db.New()
 	event, err := q.GetEventByIdQuery(ctx, conn, eventId)
 	if err == pgx.ErrNoRows {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Event not found"})
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Event not found",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Attempted to fetch event which does not exist", err)
 		return
-	} else if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+	}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
 		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Request event does not exist", err)
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Event fetched successfully",
 		"event":   event,
