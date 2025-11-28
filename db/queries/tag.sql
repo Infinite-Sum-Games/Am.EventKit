ece3c754511b72cf9c91134fb7961ce662d0dd5f
akshayks13 <akshayks1005@gmail.com>
2025-11-28 18:29:02 +0530

    feat:added GetTagbyid and create in sql

--- db/queries/tag.sql
+++ db/queries/tag.sql
@@ -4,3 +4,15 @@ SELECT
     name,
     abbreviation
 FROM tags;
+
+-- name: GetTagByIDQuery :one
+SELECT 
+    id,
+    name,
+    abbreviation
+FROM tags
+WHERE id = $1;
+
+-- name: CreateTagQuery :exec
+INSERT INTO tags (name, abbreviation)
+VALUES ($1, $2);
