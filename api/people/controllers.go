c72a536fb5dfd4a9c99b71868d5bd364dbd01c06
Nandgopal-R <nandgopalrnair@gmail.com>
2025-11-30 03:30:23 +0530

    feat: add FetchPeopleByDepartment controller

--- api/people/controllers.go
+++ api/people/controllers.go
@@ -38,3 +38,33 @@ func FetchAllPeople(c *gin.Context) {
 	})
 	pkg.Log.SuccessCtx(c)
 }
+
+func FetchPeopleByDepartment(c *gin.Context) {
+	dept := c.Param("dept")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		pkg.Log.ErrorCtx(c, "[PEOPLE-ERROR]: Failed to acquire DB connection", err)
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+
+	people, err := q.FetchPeopleByDepartmentQuery(ctx, conn, dept)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Oops! Something happened. Please try again later"})
+		pkg.Log.ErrorCtx(c, "[PEOPLE-ERROR]: Failed to fetch people by department", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "People list fetched successfully",
+		"people":  people,
+	})
+	pkg.Log.SuccessCtx(c)
+}
