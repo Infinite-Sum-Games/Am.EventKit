f9471b503c97485756e4751ed0ef140efde02e68
akshayks13 <akshayks1005@gmail.com>
2025-11-29 13:59:05 +0530

    fix:Removed unused model and upgraded sqlc

--- models/tag.go
+++ models/tag.go
@@ -1,13 +1,5 @@
 package models
 
-import "github.com/google/uuid"
-
-type Tag struct {
-	ID           uuid.UUID `json:"id"`
-	Name         string    `json:"name"`
-	Abbreviation string    `json:"abbreviation"`
-}
-
 type CreateTagRequest struct {
 	Name         string `json:"name" binding:"required"`
 	Abbreviation string `json:"abbreviation" binding:"required"`
