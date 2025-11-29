46ecf78af642be85bec176656ab7bce107325e48
Nandgopal-R <nandgopalrnair@gmail.com>
2025-11-29 18:41:19 +0530

    feat: add people routes

---
+++ api/people/routes.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	mw "github.com/Thanus-Kumaar/anokha-2025-backend/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func PeopleRoutes(r *gin.RouterGroup) {
+	r.GET("/people", mw.Auth, FetchAllPeople)
+	r.GET("/people/department/:dept", mw.Auth, FetchPeopleByDepartment)
+	r.GET("/people/event", mw.Auth, FetchPeopleByEvent)
+	r.GET("/people/day/:day", mw.Auth, FetchPeopleByDay)
+
+	r.POST("/people", mw.Auth, AddNewPerson)
+
+	r.PUT("/people/:id", mw.Auth, UpdatePersonDetails)
+
+	r.DELETE("/people/:id", mw.Auth, DeletePerson)
+}
