c835e6e64d6c5b2540197dc219ffedd01c0bd061
Nandgopal-R <nandgopalrnair@gmail.com>
2025-11-30 23:03:30 +0530

    fix: mend query for AddNewPersonQuery

--- db/queries/people.sql
+++ db/queries/people.sql
@@ -11,19 +11,34 @@ WHERE organizer.name = $1;
 SELECT * FROM people
 INNER JOIN people_to_event_mapping ON people.id = people_to_event_mapping.person_id
 INNER JOIN event ON people_to_event_mapping.event_id = event.id
-WHERE event.id = $1;
+WHERE event.name = $1;
 
 -- name: FetchPeopleByDayQuery :many
 SELECT * FROM people WHERE ARRAY_CONTAINS(event_day, $1);
 
 -- name: AddNewPersonQuery :one
-INSERT INTO people (name, phone_number, profession, email) VALUES ($1, $2, $3, $4)
+INSERT INTO people (
+  name, 
+  phone_number, 
+  profession, 
+  email) VALUES ($1, $2, $3, $4)
 RETURNING
 id,
 name,
 phone_number,
 profession,
 email;
+
+-- name: MapPersonToEventQuery :one
+INSERT INTO people_to_event_mapping (
+  event_id, 
+  person_id, 
+  event_day) VALUES ($1, $2, $3)
+RETURNING
+id,
+event_id,
+person_id,
+event_day;
 
 -- name: UpdatePersonDetailsQuery :one
 UPDATE people SET 
