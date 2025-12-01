e2388862655bca6091b961841d282c73300ff711
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-01 23:50:08 +0530

    fix: correct validation for UpdatePerson

--- models/people.go
+++ models/people.go
@@ -44,9 +44,17 @@ type UpdatePersonEventRequest struct {
 
 func (p UpdatePersonEventRequest) Validate() error {
 	return v.ValidateStruct(&p,
-		v.Field(&p.Name, v.Required, v.Length(2, 100)),
-		v.Field(&p.PhoneNumber, v.Required, v.Match(regexp.MustCompile(`^[0-9]{10}$`))),
-		v.Field(&p.Profession, v.When(p.Profession != nil,
+		v.Field(&p.Name, v.When(
+			p.Name != "",
+			v.Length(2, 100),
+		)),
+		v.Field(&p.PhoneNumber, v.When(
+			p.PhoneNumber != "",
+			v.Length(10, 10),
+			v.Match(regexp.MustCompile(`^[0-9]{10}$`)),
+		)),
+		v.Field(&p.Profession, v.When(
+			p.Profession != nil,
 			v.Length(1, 100),
 		)),
 		v.Field(&p.Email, v.When(p.Email != nil,
