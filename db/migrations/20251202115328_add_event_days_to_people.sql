dc11a3c510ca8f92ea2586314d89efc8f46f2722
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-02 23:51:22 +0530

    fix: mend column name

--- db/migrations/20251202115328_add_event_days_to_people.sql
+++ db/migrations/20251202115328_add_event_days_to_people.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE people_to_event_mapping
-    ADD COLUMN event_days integer;
+    ADD COLUMN event_day INTEGER[];
 -- +goose StatementEnd
 
 -- +goose Down
