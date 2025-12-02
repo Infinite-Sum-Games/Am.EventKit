06916feef80821bc9c9cd7721c965b5f4be3a02d
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-03 01:44:05 +0530

    db: Remove goose down statement in new migration

--- db/migrations/20251202115328_add_event_days_to_people.sql
+++ db/migrations/20251202115328_add_event_days_to_people.sql
@@ -3,9 +3,3 @@
 ALTER TABLE people_to_event_mapping
     ADD COLUMN event_day INTEGER[];
 -- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE people_to_event_mapping
-    DROP COLUMN event_days;
--- +goose StatementEnd
