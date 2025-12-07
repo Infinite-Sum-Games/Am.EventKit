ef3a6528beeaf089d1c70a30e6630600329808fb
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-07 07:52:04 +0530

    ref: View events controllers uuid

--- api/event/view-noauth.controllers.go
+++ api/event/view-noauth.controllers.go
@@ -11,7 +11,6 @@ import (
 	db "github.com/Thanus-Kumaar/anokha-2025-backend/db/gen"
 	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func FetchAllEvents(c *gin.Context) {
@@ -48,12 +47,8 @@ func FetchAllEvents(c *gin.Context) {
 
 func FetchEventById(c *gin.Context) {
 	eventIdStr := c.Param("eventId")
-	eventId, err := uuid.Parse(eventIdStr)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"message": "Request not processed due to invalid parameters",
-		})
-		pkg.Log.ErrorCtx(c, "[EVENT-ERROR]: Invalid event ID", err)
+	eventId, ok := pkg.GrabUuid(c, eventIdStr, "EVENT", "event")
+	if !ok {
 		return
 	}
 
