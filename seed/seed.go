c663e6cb021d26c50f4d9650de70d47585ac5648
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-08 00:54:19 +0530

    fix: Student seed bug

--- seed/seed.go
+++ seed/seed.go
@@ -18,7 +18,7 @@ func SeedStudents(conn *pgx.Conn) error {
 	q := db.New()
 
 	students, _ := q.ViewStudentSeedQuery(context.Background(), conn)
-	if len(students) > 0 {
+	if students[0] > 0 {
 		pkg.Log.Info("Students already seeded, skipping...")
 		return nil
 	}
