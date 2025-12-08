85481f87a7907414f54dc2be14ede113bfc2539b
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-08 22:34:02 +0530

    feat: add GetParticipantAnalytics controller

--- api/analytics/controllers.go
+++ api/analytics/controllers.go
@@ -44,7 +44,35 @@ func GetRevenueAnalytics(c *gin.Context) {
 }
 
 func GetParticipantAnalytics(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+		pkg.Log.FatalCtx(c, "[ANALYTICS-FATAL]: Failed to acquire DB connection", err)
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+
+	participantsAnalytics, err := q.GetParticipantAnalyticsQuery(ctx, conn)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+		pkg.Log.ErrorCtx(c, "[ANALYTICS-ERROR]: Failed to get participants analytics", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":                "Successfully fetched participants analytics",
+		"participants-analytics": participantsAnalytics,
+	})
+	pkg.Log.SuccessCtx(c)
 }
 
 func GetRegistrationAnalytics(c *gin.Context) {
