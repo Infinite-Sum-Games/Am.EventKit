43ab0388aa345eaadda0ada1bb41e32a69d688d5
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-08 18:59:28 +0530

    feat: add materialized view for participants analytics

--- db/migrations/20251206143217_add_analytics_materialized_view.sql
+++ db/migrations/20251206143217_add_analytics_materialized_view.sql
@@ -1,13 +1,20 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE MATERIALIZED VIEW IF NOT EXISTS revenue_analytics AS
-SELECT b.registration_fee, 
-e.name, 
-e.event_type, 
-es.event_date,
-es.start_time,
-es.end_time,
-o.name
+SELECT b.registration_fee AS booking_fee, 
+e.id AS event_id,
+e.name AS event_name, 
+e.event_type AS event_type,
+es.event_date AS event_date,
+es.start_time AS start_time,
+es.end_time AS end_time,
+o.id AS organizer_id,
+o.name AS organizer_name,
+SUM(b.registration_fee) OVER () AS total_revenue,
+SUM(b.registration_fee) OVER (PARTITION BY e.id) AS revenue_per_event,
+SUM(b.registration_fee) OVER (PARTITION BY es.event_date) AS revenue_per_date,
+SUM(b.registration_fee) OVER (PARTITION BY e.event_type) AS revenue_per_event_type,
+SUM(b.registration_fee) OVER (PARTITION BY o.id) AS revenue_per_organizer
 FROM bookings AS b
 INNER JOIN event AS e on b.event_id = e.id
 INNER JOIN event_schedule AS es ON e.id = es.event_id
@@ -17,9 +24,21 @@ INNER JOIN organizer AS o ON etom.organizer_id = o.id;
 
 -- +goose StatementBegin
 CREATE MATERIALIZED VIEW IF NOT EXISTS participant_analytics AS
-SELECT s.name 
+SELECT s.id AS student_id,
+s.name AS student_name,
+s.email AS student_email,
+e.id AS event_id,
+e.name AS event_name,
+e.event_type AS event_type,
+COUNT(b.id) OVER () AS global_total_bookings,
+COUNT(b.id) OVER (PARTITION BY e.id) AS participants_per_event,
+COUNT(b.id) OVER (PARTITION BY o.id) AS bookings_per_organizer
 FROM bookings AS b
 INNER JOIN student AS s ON b.student_id = s.id
+INNER JOIN event AS e ON b.event_id = e.id
+INNER JOIN event_to_organizer_mapping AS etom ON e.id = etom.event_id
+INNER JOIN organizer AS o ON etom.organizer_id = o.id;
+-- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
