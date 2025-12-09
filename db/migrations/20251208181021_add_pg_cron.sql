5697e8c1c07be5d578855daf3f2c3d70a568a228
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-09 05:44:41 +0530

    fix: make pg_cron to refresh every 30 minutes

--- db/migrations/20251208181021_add_pg_cron.sql
+++ db/migrations/20251208181021_add_pg_cron.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 SELECT cron.schedule(
     'refresh_revenue_every_30m',
-    '* * * * *',
+    '*/30 * * * *',
     'REFRESH MATERIALIZED VIEW CONCURRENTLY revenue_analytics'
 );
 -- +goose StatementEnd
@@ -10,7 +10,7 @@ SELECT cron.schedule(
 -- +goose StatementBegin
 SELECT cron.schedule(
     'refresh_participant_every_30m',
-    '* * * * *',
+    '*/30 * * * *',
     'REFRESH MATERIALIZED VIEW CONCURRENTLY participant_analytics'
 );
 -- +goose StatementEnd
@@ -18,7 +18,7 @@ SELECT cron.schedule(
 -- +goose StatementBegin
 SELECT cron.schedule(
     'refresh_registrations_every_30m',
-    '* * * * *',
+    '*/30 * * * *',
     'REFRESH MATERIALIZED VIEW CONCURRENTLY registrations_analytics'
 );
 -- +goose StatementEnd
@@ -26,7 +26,7 @@ SELECT cron.schedule(
 -- +goose StatementBegin
 SELECT cron.schedule(
     'refresh_people_every_30m',
-    '* * * * *',
+    '*/30 * * * *',
     'REFRESH MATERIALIZED VIEW CONCURRENTLY people_analytics'
 );
 -- +goose StatementEnd
