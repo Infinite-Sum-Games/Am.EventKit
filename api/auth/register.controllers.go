4504f7697c81f52e3ac270bc93d80042e889fc8f
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-12 02:39:09 +0530

    fix: OTP year in registration

--- api/auth/register.controllers.go
+++ api/auth/register.controllers.go
@@ -157,7 +157,7 @@ func RegisterUserAccount(c *gin.Context) {
 
 	err = mail.Mail.Enqueue(&mail.EmailRequest{
 		To:      []string{req.Email},
-		Subject: "OTP - Anokha 2025",
+		Subject: "OTP - Anokha 2026",
 		Type:    "otp",
 		Data: &mail.OTPTemplateData{
 			UserName: req.Name,
@@ -271,7 +271,7 @@ func VerifyUserOtp(c *gin.Context) {
 
 	err = mail.Mail.Enqueue(&mail.EmailRequest{
 		To:      []string{result.Email},
-		Subject: "Welcome to Anokha 2025",
+		Subject: "Welcome to Anokha 2026",
 		Type:    "welcome",
 		Data: &mail.WelcomeTemplateData{
 			UserName: result.Name,
@@ -328,7 +328,7 @@ func ResendUserOtp(c *gin.Context) {
 	// Resend OTP via Mail
 	emailReq := mail.EmailRequest{
 		To:      []string{email},
-		Subject: fmt.Sprintf("Resend OTP - Anokha 2025 - %d", time.Now().UnixMilli()),
+		Subject: fmt.Sprintf("Resend OTP - Anokha 2026 - %d", time.Now().UnixMilli()),
 		Type:    "otp",
 		Data: &mail.OTPTemplateData{
 			UserName: results.Name,
