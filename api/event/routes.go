db578dfe53dd54a5fa11382eab533c69c8d203eb
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-13 23:25:42 +0530

    feat: New routes for admin pushed

--- api/event/routes.go
+++ api/event/routes.go
@@ -6,15 +6,24 @@ import (
 )
 
 func EventRoutes(r *gin.RouterGroup) {
+	// User event controllers
 	r.GET("/", FetchAllEvents)
 	r.GET("/:eventId", FetchEventById)
 	r.GET("/auth/", mw.Auth, mw.CheckUser, FetchAllEventsWithAuth)
 	r.GET("/auth/:eventId", mw.Auth, mw.CheckUser, FetchEventByEventIdWithAuth)
+	r.PUT("/favourite/:eventId", mw.Auth, mw.CheckUser, StarEvent)
+	r.DELETE("/favourite/:eventId", mw.Auth, mw.CheckUser, UnstarEvent)
 
+	// Admin event controllers
 	r.GET("/admin/new", NewEvent)
+	r.POST("/admin/poster", AddEventPoster)
+	r.DELETE("/admin/poster", DeleteEventPoster)
+	r.POST("/admin/", AddEventDimension)
+	r.POST("/admin/toggle", AddEventToggles)
+	r.POST("/admin/")
+
+	// Deprecated routes; under refactoring
 	r.PUT("/:eventId", mw.Auth, mw.CheckAdmin, EditEvent)
 	r.PUT("/:eventId/toggle-status", mw.Auth, mw.CheckAdmin, ToggleEventStatus)
 
-	r.PUT("/favourite/:eventId", mw.Auth, mw.CheckUser, StarEvent)
-	r.DELETE("/favourite/:eventId", mw.Auth, mw.CheckUser, UnstarEvent)
 }
