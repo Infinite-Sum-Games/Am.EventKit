2a2f5da1b81cca293f3332c06bd07a4d3041ed6e
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-14 11:57:59 +0530

    more cleanup

--- api/event/admin.controllers.go
+++ api/event/admin.controllers.go
@@ -1,139 +1,156 @@
 package api
 
-// func NewEvent(c *gin.Context) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-//
-// 	conn, err := cmd.DBPool.Acquire(ctx)
-// 	if pkg.HandleDbAcquireErr(c, err, "EVENT") {
-// 		return
-// 	}
-// 	defer conn.Release()
-//
-// 	q := db.New()
-// 	result, err := q.NewUntitledEventQuery(ctx, conn,
-// 		db.NewUntitledEventQueryParams{
-// 			Name:        fmt.Sprintf("Untitled %s", ksuid.New().String()),
-// 			Blurb:       "",
-// 			Description: "",
-// 			Price: pgtype.Numeric{
-// 				Valid: true,
-// 				Int:   big.NewInt(int64(0)),
-// 			},
-// 			IsPerHead: true,
-// 			Rules:     "",
-// 			EventType: db.EventTypeEnumEVENT,
-// 			IsGroup:   false,
-// 			MinTeamsize: pgtype.Int4{
-// 				Valid: true,
-// 				Int32: 1,
-// 			},
-// 			MaxTeamsize: pgtype.Int4{
-// 				Valid: true,
-// 				Int32: 1,
-// 			},
-// 			TotalSeats:     0,
-// 			EventStatus:    db.EventStatusEnumCLOSED,
-// 			EventMode:      db.EventModeEnumOFFLINE,
-// 			AttendanceMode: db.AttendanceModeEnumSOLO,
-// 			IsTechnical: pgtype.Bool{
-// 				Valid: true,
-// 				Bool:  false,
-// 			},
-// 		})
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"message": "Oops! Something happened. Please try again later",
-// 		})
-// 		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to create new event", err)
-// 		return
-// 	}
-//
-// 	eventIsCompleted := result.EventStatus == db.EventStatusEnumCOMPLETED
-// 	eventIsActive := result.EventStatus == db.EventStatusEnumACTIVE
-// 	eventIsOffline := result.EventMode == db.EventModeEnumOFFLINE
-//
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message":         "New event created successfully",
-// 		"id":              result.ID.String(),
-// 		"name":            result.Name,
-// 		"blurb":           result.Blurb,
-// 		"description":     result.Description,
-// 		"poster_url":      result.CoverImageUrl.String,
-// 		"price":           result.Price.Int,
-// 		"is_per_head":     result.IsPerHead,
-// 		"rules":           result.Rules,
-// 		"is_group":        result.IsGroup,
-// 		"min_teamsize":    result.MinTeamsize.Int32,
-// 		"max_teamsize":    result.MaxTeamsize.Int32,
-// 		"seat_count":      result.TotalSeats,
-// 		"event_type":      result.EventType,
-// 		"is_technical":    result.IsTechnical,
-// 		"is_offline":      eventIsOffline,
-// 		"attendance_mode": result.AttendanceMode,
-// 		"is_published":    eventIsActive || eventIsCompleted,
-// 		"event_status":    eventIsCompleted,
-// 		"people":          []string{},
-// 		"organizers":      []string{},
-// 		"tags":            []string{},
-// 		"schedules":       []string{},
-// 	})
-// 	pkg.Log.SuccessCtx(c)
-// }
-//
-// // Name, Blurb, Description, Rules, Price
-// func AddEventDetails(c *gin.Context) {
-// 	eventId, ok := pkg.GrabUuid(c, c.Param("eventId"), "ADMIN-EVENT", "Event")
-// 	if !ok {
-// 		return
-// 	}
-//
-// 	req, ok := pkg.ValidateRequest[models.AddEventDetailsRequest](c)
-// 	if !ok {
-// 		return
-// 	}
-//
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-//
-// 	conn, err := cmd.DBPool.Acquire(ctx)
-// 	if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
-// 		return
-// 	}
-//
-// 	q := db.New()
-// 	result, err := q.AddEventDetailsQuery(ctx, conn, db.AddEventDetailsQueryParams{
-// 		ID:          eventId,
-// 		Name:        req.Name,
-// 		Blurb:       req.Blurb,
-// 		Description: req.Description,
-// 		Rules:       req.Rules,
-// 		Price: pgtype.Numeric{
-// 			Int:   big.NewInt(int64(req.Price)),
-// 			Valid: true,
-// 		},
-// 		IsPerHead: req.IsPerHead,
-// 	})
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"message": "Oops! Something happened. Please try again later",
-// 		})
-// 		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to add event details", err)
-// 		return
-// 	}
-//
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message":     "Event updated successfully",
-// 		"id":          result.ID.String(),
-// 		"name":        result.Name,
-// 		"blurb":       result.Blurb,
-// 		"description": result.Description,
-// 		"rules":       result.Rules,
-// 		"price":       result.Price,
-// 		"is_per_head": result.IsPerHead,
-// 	})
-// 	pkg.Log.SuccessCtx(c)
-// }
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"net/http"
+	"time"
+
+	"github.com/Thanus-Kumaar/anokha-2025-backend/cmd"
+	db "github.com/Thanus-Kumaar/anokha-2025-backend/db/gen"
+	"github.com/Thanus-Kumaar/anokha-2025-backend/models"
+	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/segmentio/ksuid"
+)
+
+func NewEvent(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if pkg.HandleDbAcquireErr(c, err, "EVENT") {
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+	result, err := q.NewUntitledEventQuery(ctx, conn,
+		db.NewUntitledEventQueryParams{
+			Name:        fmt.Sprintf("Untitled %s", ksuid.New().String()),
+			Blurb:       "",
+			Description: "",
+			Price: pgtype.Numeric{
+				Valid: true,
+				Int:   big.NewInt(int64(0)),
+			},
+			IsPerHead: true,
+			Rules:     "",
+			EventType: db.EventTypeEnumEVENT,
+			IsGroup:   false,
+			MinTeamsize: pgtype.Int4{
+				Valid: true,
+				Int32: 1,
+			},
+			MaxTeamsize: pgtype.Int4{
+				Valid: true,
+				Int32: 1,
+			},
+			TotalSeats:     0,
+			EventStatus:    db.EventStatusEnumCLOSED,
+			EventMode:      db.EventModeEnumOFFLINE,
+			AttendanceMode: db.AttendanceModeEnumSOLO,
+			IsTechnical: pgtype.Bool{
+				Valid: true,
+				Bool:  false,
+			},
+		})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to create new event", err)
+		return
+	}
+
+	eventIsCompleted := result.EventStatus == db.EventStatusEnumCOMPLETED
+	eventIsActive := result.EventStatus == db.EventStatusEnumACTIVE
+	eventIsOffline := result.EventMode == db.EventModeEnumOFFLINE
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":         "New event created successfully",
+		"id":              result.ID.String(),
+		"name":            result.Name,
+		"blurb":           result.Blurb,
+		"description":     result.Description,
+		"poster_url":      result.CoverImageUrl.String,
+		"price":           result.Price.Int,
+		"is_per_head":     result.IsPerHead,
+		"rules":           result.Rules,
+		"is_group":        result.IsGroup,
+		"min_teamsize":    result.MinTeamsize.Int32,
+		"max_teamsize":    result.MaxTeamsize.Int32,
+		"seat_count":      result.TotalSeats,
+		"event_type":      result.EventType,
+		"attendance_mode": result.AttendanceMode,
+		"is_technical":    result.IsTechnical,
+		"is_offline":      eventIsOffline,
+		"is_published":    eventIsActive || eventIsCompleted,
+		"is_completed":    eventIsCompleted,
+		"people":          []string{},
+		"organizers":      []string{},
+		"tags":            []string{},
+		"schedules":       []string{},
+	})
+	pkg.Log.SuccessCtx(c)
+}
+
+// Name, Blurb, Description, Rules, Price
+func AddEventDetails(c *gin.Context) {
+	eventId, ok := pkg.GrabUuid(c, c.Param("eventId"), "ADMIN-EVENT", "Event")
+	if !ok {
+		return
+	}
+
+	req, ok := pkg.ValidateRequest[models.AddEventDetailsRequest](c)
+	if !ok {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
+		return
+	}
+
+	q := db.New()
+	result, err := q.AddEventDetailsQuery(ctx, conn, db.AddEventDetailsQueryParams{
+		ID:          eventId,
+		Name:        req.Name,
+		Blurb:       req.Blurb,
+		Description: req.Description,
+		Rules:       req.Rules,
+		Price: pgtype.Numeric{
+			Int:   big.NewInt(int64(req.Price)),
+			Valid: true,
+		},
+		IsPerHead: req.IsPerHead,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to add event details", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":     "Event updated successfully",
+		"id":          result.ID.String(),
+		"name":        result.Name,
+		"blurb":       result.Blurb,
+		"description": result.Description,
+		"rules":       result.Rules,
+		"price":       result.Price,
+		"is_per_head": result.IsPerHead,
+	})
+	pkg.Log.SuccessCtx(c)
+}
+
 //
 // func AddEventPoster(c *gin.Context) {
 // 	eventId, ok := pkg.GrabUuid(c, c.Param("eventId"), "ADMIN-EVENT", "Event")
