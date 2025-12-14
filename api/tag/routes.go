60982da48719049a30120eed15712c08af9a3186
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-15 04:04:29 +0530

    tags behind auth wall

--- api/tag/routes.go
+++ api/tag/routes.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TagRoutes(r *gin.RouterGroup) {
-	r.GET("/", GetAllEventTags)
+	r.GET("/", mw.Auth, mw.CheckAdmin, GetAllEventTags)
 	r.POST("/", mw.Auth, mw.CheckAdmin, CreateEventTag)
 	r.PUT("/:tagId", mw.Auth, mw.CheckAdmin, EditEventTag)
 	r.DELETE("/:tagId", mw.Auth, mw.CheckAdmin, DeleteEventTag)
