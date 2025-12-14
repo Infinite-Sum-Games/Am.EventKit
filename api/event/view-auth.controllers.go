5dad75aef55268062753ca2482f13cc2cddff248
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-14 14:08:38 +0530

    send is_full in get-event-by-event-id

--- api/event/view-auth.controllers.go
+++ api/event/view-auth.controllers.go
@@ -105,6 +105,7 @@ func FetchEventByEventIdWithAuth(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Event successfully fetched",
 		"event":   event,
+		"is_full": event.SeatsFilled == event.TotalSeats,
 	})
 	pkg.Log.SuccessCtx(c)
 }
