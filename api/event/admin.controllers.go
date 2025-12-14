32df88c287ed21ce6c8168ed62b93ddfb6d38669
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-14 13:27:32 +0530

    connect and disconnect event tag mapping

--- api/event/admin.controllers.go
+++ api/event/admin.controllers.go
@@ -397,8 +397,8 @@ func ConnectEventAndOrganizer(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"message":      "Successfully connected event with organizer",
-		"id":           result.EventID,
-		"organizer_id": result.OrganizerID,
+		"id":           result.EventID.String(),
+		"organizer_id": result.OrganizerID.String(),
 	})
 	pkg.Log.SuccessCtx(c)
 }
@@ -454,50 +454,101 @@ func DisconnectEventAndOrganizer(c *gin.Context) {
 	pkg.Log.SuccessCtx(c)
 }
 
-//	func ConnectEventAndTags(c *gin.Context) {
-//		req, ok := pkg.ValidateRequest[models.ConnectEventAndTagsRequest](c)
-//		if !ok {
-//			return
-//		}
-//
-//		eventId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Event")
-//		if !ok {
-//			return
-//		}
-//		tagId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Tag")
-//		if !ok {
-//			return
-//		}
-//
-//		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-//		defer cancel()
-//
-//		conn, err := cmd.DBPool.Acquire(ctx)
-//		if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
-//			return
-//		}
-//		defer conn.Release()
-//
-//		q := db.New()
-//		result, err := q.ConnectEventAndTagsQuery(ctx, conn, db.ConnectEventAndTagsQueryParams{
-//			EventID: eventId,
-//			TagID:   tagId,
-//		})
-//		if err != nil {
-//			c.JSON(http.StatusInternalServerError, gin.H{
-//				"": "",
-//			})
-//			pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to add tag for event", err)
-//			return
-//		}
-//
-//		c.JSON(http.StatusOK, gin.H{
-//			"message": "Tag added for event",
-//			"id":      result.EventID.String(),
-//			"tag_id":  result.TagID.String(),
-//		})
-//		pkg.Log.SuccessCtx(c)
-//	}
+func ConnectEventAndTags(c *gin.Context) {
+	req, ok := pkg.ValidateRequest[models.ConnectEventAndTagsRequest](c)
+	if !ok {
+		return
+	}
+
+	eventId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Event")
+	if !ok {
+		return
+	}
+	tagId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Tag")
+	if !ok {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+	result, err := q.ConnectEventAndTagsQuery(ctx, conn, db.ConnectEventAndTagsQueryParams{
+		EventID: eventId,
+		TagID:   tagId,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"": "",
+		})
+		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to add tag for event", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Tag added for event",
+		"id":      result.EventID.String(),
+		"tag_id":  result.TagID.String(),
+	})
+	pkg.Log.SuccessCtx(c)
+}
+
+func DisonnectEventAndTags(c *gin.Context) {
+	req, ok := pkg.ValidateRequest[models.DisconnectEventAndTagsRequest](c)
+	if !ok {
+		return
+	}
+
+	eventId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Event")
+	if !ok {
+		return
+	}
+	tagId, ok := pkg.GrabUuid(c, req.TagId, "ADMIN-EVENT", "Tag")
+	if !ok {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
+		return
+	}
+	defer conn.Release()
+
+	q := db.New()
+	_, err = q.DisconnectEventAndTagsQuery(ctx, conn,
+		db.DisconnectEventAndTagsQueryParams{
+			EventID: eventId,
+			TagID:   tagId,
+		})
+	if err == pgx.ErrNoRows {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No mapping found for given event and tag",
+		})
+		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to disconnect event and tag", err)
+		return
+	}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later",
+		})
+		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to disconnect event and tag", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully removed tag for event",
+	})
+	pkg.Log.SuccessCtx(c)
+}
 
 func ConnectEventAndPeople(c *gin.Context) {
 	req, ok := pkg.ValidateRequest[models.ConnectEventAndPeopleRequest](c)
@@ -539,8 +590,8 @@ func ConnectEventAndPeople(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"messasge":  "Successfully connected event and person",
-		"id":        result.EventID,
-		"person_id": result.PersonID,
+		"id":        result.EventID.String(),
+		"person_id": result.PersonID.String(),
 	})
 	pkg.Log.SuccessCtx(c)
 }
@@ -592,57 +643,6 @@ func DisconnectEventAndPeople(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Successfully disconnected event and person",
-	})
-	pkg.Log.SuccessCtx(c)
-}
-
-func DisonnectEventAndTags(c *gin.Context) {
-	req, ok := pkg.ValidateRequest[models.DisconnectEventAndTagsRequest](c)
-	if !ok {
-		return
-	}
-
-	eventId, ok := pkg.GrabUuid(c, req.EventId, "ADMIN-EVENT", "Event")
-	if !ok {
-		return
-	}
-	tagId, ok := pkg.GrabUuid(c, req.TagId, "ADMIN-EVENT", "Tag")
-	if !ok {
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	conn, err := cmd.DBPool.Acquire(ctx)
-	if !pkg.HandleDbAcquireErr(c, err, "ADMIN-EVENT") {
-		return
-	}
-	defer conn.Release()
-
-	q := db.New()
-	_, err = q.DisconnectEventAndTagsQuery(ctx, conn,
-		db.DisconnectEventAndTagsQueryParams{
-			EventID: eventId,
-			TagID:   tagId,
-		})
-	if err == pgx.ErrNoRows {
-		c.JSON(http.StatusNotFound, gin.H{
-			"message": "No mapping found for given event and tag",
-		})
-		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to disconnect event and tag", err)
-		return
-	}
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Oops! Something happened. Please try again later",
-		})
-		pkg.Log.ErrorCtx(c, "[ADMIN-EVENT-ERROR]: Failed to disconnect event and tag", err)
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "",
 	})
 	pkg.Log.SuccessCtx(c)
 }
