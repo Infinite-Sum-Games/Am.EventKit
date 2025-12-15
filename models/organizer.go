68baeb9f1a85df4ddaad0f0422476d18796ad318
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-15 09:41:31 +0530

    fix: Remove password in the right place

--- models/organizer.go
+++ models/organizer.go
@@ -8,6 +8,7 @@ import (
 type CreateOrganizerRequest struct {
 	Name          string `json:"name"`
 	Email         string `json:"email"`
+	Password      string `json:"password"`
 	OrgType       string `json:"org_type"` // DEPARTMENT | CLUB
 	StudentHead   string `json:"student_head"`
 	StudentCoHead string `json:"student_co_head"` // optional
@@ -20,11 +21,33 @@ func (r CreateOrganizerRequest) Validate() error {
 		// Required fields
 		v.Field(&r.Name, v.Required, v.RuneLength(3, 100)),
 		v.Field(&r.Email, v.Required, is.Email),
+		v.Field(&r.Password, v.Required, v.RuneLength(6, 100)),
 		v.Field(&r.OrgType, v.Required, v.In("DEPARTMENT", "CLUB")),
 		v.Field(&r.StudentHead, v.Required, v.RuneLength(3, 100)),
 		v.Field(&r.FacultyHead, v.Required, v.RuneLength(3, 100)),
 
 		// Optional fields
+		v.Field(&r.StudentCoHead, v.When(r.StudentCoHead != "", v.RuneLength(3, 100))),
+	)
+}
+
+type EditOrganizerRequest struct {
+	Name          string `json:"name"`
+	Email         string `json:"email"`
+	OrgType       string `json:"org_type"` // DEPARTMENT | CLUB
+	StudentHead   string `json:"student_head"`
+	StudentCoHead string `json:"student_co_head"` // optional
+	FacultyHead   string `json:"faculty_head"`
+}
+
+func (r EditOrganizerRequest) Validate() error {
+	return v.ValidateStruct(&r,
+
+		v.Field(&r.Name, v.Required, v.RuneLength(3, 100)),
+		v.Field(&r.Email, v.Required, is.Email),
+		v.Field(&r.OrgType, v.Required, v.In("DEPARTMENT", "CLUB")),
+		v.Field(&r.StudentHead, v.Required, v.RuneLength(3, 100)),
+		v.Field(&r.FacultyHead, v.Required, v.RuneLength(3, 100)),
 		v.Field(&r.StudentCoHead, v.When(r.StudentCoHead != "", v.RuneLength(3, 100))),
 	)
 }
