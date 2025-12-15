58b46ce90bc0f3fbd901e049c2fe3b14c99c27c7
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-12-16 00:01:48 +0530

    added ps for hackathon to register model

--- models/team.go
+++ models/team.go
@@ -8,6 +8,7 @@ import (
 type TeamBookingRequest struct {
 	TeamName    string       `json:"team_name" binding:"required"`
 	TeamMembers []TeamMember `json:"team_members" binding:"required"`
+	ProblemStmt string       `json:"ps,omitempty"`
 }
 
 type TeamMember struct {
@@ -36,5 +37,7 @@ func (e TeamBookingRequest) Validate() error {
 				return nil
 			})),
 		),
+		// asuming the only metadata we get is related to problem statement type
+		v.Field(&e.ProblemStmt, v.In("agentic_ai", "generative_ai", "aiot")),
 	)
 }
