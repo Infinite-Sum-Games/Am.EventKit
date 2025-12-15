bbf4d4d452d47eff52dd2afec972950a93bafeaf
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-15 10:25:04 +0530

    maximum menon

--- db/gen/event.sql.go
+++ db/gen/event.sql.go
@@ -26,8 +26,7 @@ SELECT
     (e.seats_filled = e.total_seats) AS is_full,
 
     COALESCE(
-        JSONB_AGG(DISTINCT t.abbreviation) FILTER (WHERE t.id IS NOT NULL),
-        '[]'::jsonb
+      array_agg(DISTINCT t.name) FILTER (WHERE t.id IS NOT NULL)
     ) AS tags,
 
     e.price AS event_price,
@@ -263,11 +262,7 @@ SELECT
     ) AS schedules,
 
     COALESCE(
-      JSONB_AGG(DISTINCT JSONB_BUILD_OBJECT(
-        'tag_name', t.name,
-        'tag_abbreviation', t.abbreviation
-      )) FILTER (WHERE t.id IS NOT NULL),
-      '[]'::jsonb
+      array_agg(DISTINCT t.name) FILTER (WHERE t.id IS NOT NULL)
     ) AS tags,
 
     COALESCE(
@@ -372,8 +367,7 @@ SELECT
     e.is_technical,
 
     COALESCE(
-        JSONB_AGG(DISTINCT t.abbreviation) FILTER (WHERE t.id IS NOT NULL),
-        '[]'::jsonb
+      array_agg(DISTINCT t.name) FILTER (WHERE t.id IS NOT NULL)
     ) AS tags,
 
     e.price AS event_price,
