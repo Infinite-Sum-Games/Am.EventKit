67459414035d99d69c705486b0bbc8b9d568bfbb
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-27 00:27:08 +0530

    feat: add dispute enum

--- db/migrations/011_add_disputes_table.sql
+++ db/migrations/011_add_disputes_table.sql
@@ -1,10 +1,19 @@
 -- +goose up
 -- +goose StatementBegin
+CREATE TYPE dispute_status_enum AS ENUM (
+  'OPEN',
+  'CLOSED_AS_TRUE',
+  'CLOSED_AS_FALSE'
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS dispute (
   id UUID DEFAULT gen_random_uuid(),
-  email TEXT,
+  student_email TEXT,
   description TEXT,
   event_id UUID NOT NULL,
+  dispute_status dispute_status_enum DEFAULT 'OPEN',
   team_member_datails JSONB,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW(),
