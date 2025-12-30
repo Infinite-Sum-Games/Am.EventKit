cd2b9d41a503295e86e228f51034c9d16a9b309e
Nandgopal-R <nandgopalrnair@gmail.com>
2025-12-31 05:29:41 +0530

    fix: remove code that deletes team member on payment fail

--- api/booking/routes.go
+++ api/booking/routes.go
@@ -11,7 +11,7 @@ func BookingRoutes(r *gin.RouterGroup) {
 
 	// Booking endpoints
 	r.POST("/:eventId/book", mw.VerifyCsrf, mw.Auth, BookEvent)
-	r.POST("/verify", mw.Auth, VerifyTransaction)
+	r.POST("/verify", mw.Auth, VerifyTransactionNew)
 
 	// Fetch transactions
 	r.GET("/transactions", mw.Auth, mw.CheckAdmin, FetchAdminTransactions)
