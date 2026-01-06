06ae0c4e0afc835c9ce573f16fcf5007f5cef369
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-01-06 06:46:56 +0530

    HostelCheckIn fixed

--- api/accomodation/hostel.controllers.go
+++ api/accomodation/hostel.controllers.go
@@ -9,10 +9,15 @@ import (
 	db "github.com/Thanus-Kumaar/anokha-2025-backend/db/gen"
 	"github.com/Thanus-Kumaar/anokha-2025-backend/pkg"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func HostelCheckIn(c *gin.Context) {
-	personellId, ok := pkg.GrabUserId(c, "HOSTEL")
+	personellIdStr, ok := pkg.GrabUserId(c, "HOSTEL")
+	if !ok {
+		return
+	}
+	personellId, ok := pkg.GrabUuid(c, personellIdStr, "HOSTEL", "Personell")
 	if !ok {
 		return
 	}
@@ -29,13 +34,14 @@ func HostelCheckIn(c *gin.Context) {
 	defer conn.Release()
 
 	q := db.New()
-	ok, err := q.HostelCheckInQuery(c, conn, db.HostelCheckInQueryParams{
-		AccomodationID: ,
-		CheckedInBy: personellId,
+	details, err := q.HostelCheckInQuery(c, conn, db.HostelCheckInQueryParams{
+		HospitalityID: pgtype.Text{String: hospId, Valid: true},
+		CheckedInBy:   personellId,
 	})
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hostel check-in successful",
+		"details": details,
 	})
 	pkg.Log.SuccessCtx(c)
 }
