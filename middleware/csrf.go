2e3f5a230c86d321dee378815c8370b9eb3a903e
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-01-24 00:12:29 +0530

    fix: Drop maintainance middleware

--- middleware/csrf.go
+++ middleware/csrf.go
@@ -8,11 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/* CSRF Verification Process
-*  1. Collect CSRF token from Header and Cookie
-*  2.
- */
-
 func VerifyCsrf(c *gin.Context) {
 	csrfToken := c.Request.Header["X-Csrf-Token"]
 	if len(csrfToken) != 1 {
